6941e38900f4bfd48d512d87c626019cf88fed8a
Weronika Musial <jozefinamusial@gmail.com>
2017-03-10 23:58:32 +0100

    initial commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+    maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+
+var newName = 'Marian';
+
+if (allNames.indexOf(newName) === -1) {
+  var x = allNames.push(newName);
+}
