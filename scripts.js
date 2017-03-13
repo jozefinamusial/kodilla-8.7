e70020d3cddc9afd761294c8732470474f1693f2
Weronika Musial <jozefinamusial@gmail.com>
2017-03-13 21:58:54 +0100

    poprawione

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
-    maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 
 var allNames = femaleNames.concat(maleNames);
 
 var newName = 'Marian';
 
 if (allNames.indexOf(newName) === -1) {
-  var x = allNames.push(newName);
+  allNames.push(newName);
 }
+
+console.log(allNames);
